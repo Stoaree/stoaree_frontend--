cc6df136e74601b95bd558e9e96e017b4512fd23
Max Kelly <max@revelthat.com>
2020-01-22 10:15:09 +1100

    added questions page to router

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -6,6 +6,7 @@ import './../css/main.css';
 import HomePage from './../pages/HomePage.js';
 import LoginPage from './../pages/LoginPage.js';
 import SignupPage from './../pages/SignupPage.js';
+import QuestionsPage from './../pages/QuestionsPage.js';
 
 // Components 
 import NavBar from './../components/NavBar.js';
@@ -16,6 +17,7 @@ const AppRouter = () => {
       <div>
         <NavBar />
         <Switch>
+          <Route path="/question" component={QuestionsPage} />
           <Route path="/signup" component={SignupPage} />
           <Route path="/login" component={LoginPage} />
           <Route path="/" component={HomePage} exact={true} />
