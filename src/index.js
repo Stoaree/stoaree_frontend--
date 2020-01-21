4566f19279f1c3f8fc39129c2fc8a2fd3a28ab00
Max Kelly <max@revelthat.com>
2020-01-21 16:19:57 +1100

    signup form

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppRouter from './routes/AppRouter.js';
+
+// Redux 
+import { store } from './redux/store.js';
+import { Provider } from 'react-redux'
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}> <AppRouter /> </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
