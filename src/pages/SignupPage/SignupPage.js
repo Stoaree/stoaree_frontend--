859aca5f4e8ab5a5658fbf9ece85196d5ea6cdc9
Max Kelly <max@revelthat.com>
2020-01-22 16:25:30 +1100

    added axios post to login and signing up user and getting token back

--- src/pages/SignupPage/SignupPage.js
+++ src/pages/SignupPage/SignupPage.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
 
 // Component
 import SignupForm from '../../components/SignupForm/SignupForm.js';
 
+const cookies = new Cookies();
 class SignupPage extends React.Component {
 
   onSubmit = (values) => {
-    console.log(values.displayName);
+    axios.post("http://localhost:3001/signup", {
+      firstName: values.firstName,
+      lastName: values.lastName,
+      displayName: values.displayName,
+      email: values.email,
+      password: values.password
+    }).then(response => {
+      console.log(response)
+      if (response.status === 200) {
+        axios.post("http://localhost:3001/login", {
+          email: values.email,
+          password: values.password
+        }).then(response => {
+          const token = response.data.token;
+          cookies.set(token, true, { path: "/" })
+          console.log(cookies.get(token))
+        })
+      }
+    })
   };
 
   render() {
