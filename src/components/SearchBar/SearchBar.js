ed39686a54c5b24a284debfe8b22f3b8d0254d61
Max Kelly <max@revelthat.com>
2020-01-24 10:29:26 +1100

    search button removed and now keypress enter also added a tolowercase in search field to avoid not getting a result back

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom'
 
-
 const SearchBar = (props) => {
 
   const history = useHistory()
 
   // Redirects the user when they search
   const handleSearchButton = (e) => {
-    e.preventDefault()
-    history.push("/search")
+
+    e.target.value = e.target.value.toLowerCase();
+    if (e.key === "Enter"){
+      history.push("/search")
+    }
   }
 
   return (
     <div>
       <div className="search-div">
         <div className="inside-search-bar">
-          <input onChange={props.handleSearch} name="searchInput" type="text" placeholder="Try searching for..." />
-          <button onClick={handleSearchButton}> Submit </button>
+          <input onChange={props.handleSearch} name="searchInput" type="text" placeholder="Try searching for..." onKeyPress={handleSearchButton} />
         </div>    
       </div>
     </div>
