b5175767bd28350605fb6dc60fc8c2dff148572e
luke <l.j.dwsn@gmail.com>
2020-01-24 11:35:46 +1100

    changes

---
+++ src/pages/SearchPage/SearchPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import StoryCard from './../../components/StoryCard/StoryCard.js';
+
+class SearchPage extends React.Component {
+
+  renderStories = () => {
+    return this.props.stories.map((story) => {
+      return (
+        <div key={story._id}>
+          <StoryCard story={story}  />
+        </div>
+      )
+    })
+  };
+
+  render() {
+    return(
+      <div>
+        <h1> Search Page! </h1>
+        {this.renderStories()}
+      </div>
+    )
+  }
+};
+
+export default SearchPage;
