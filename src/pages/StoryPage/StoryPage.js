5e3e595487802a0b6ab91a1a61be8a6b8106adec
luke <l.j.dwsn@gmail.com>
2020-01-28 15:16:07 +1100

    Added profile page and comments

--- src/pages/StoryPage/StoryPage.js
+++ src/pages/StoryPage/StoryPage.js
@@ -2,45 +2,46 @@ import React from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import StoryShow from "./../../components/StoryShow/StoryShow";
+import Comment from "./../../components/Comment/Comment";
 
 class StoryPage extends React.Component {
   state = {
-    story: null
+    story: null,
+    comments: null
   };
 
   async componentDidMount() {
     const foundStory = this.props.stories.find(story => {
       return story._id === this.props.match.params.id;
     });
+
     this.setState({
-      story: foundStory
+      story: foundStory,
+      comments: foundStory.comments
     });
+  }
+
+  renderComments() {
+    return this.state.comments.map(comment => <Comment {...comment} />);
   }
 
   render() {
     const { story } = this.state;
+    const { comments } = this.state;
     console.log(story);
-    if (story) {
-      return <StoryShow story={story} />;
+    console.log(comments);
+
+    if ((story, comments)) {
+      return (
+        <div>
+          {" "}
+          <StoryShow story={story} />
+          {this.renderComments()}
+        </div>
+      );
     } else {
       return null;
     }
-
-    // const { story } = this.state;
-    // console.log(story)
-    //   return story ? (
-    //     <dl>
-    //       {" "}
-    //       <h1>Story:</h1> <dt>Title:{story.title}</dt>
-    //       <dt>Description:{story.description}</dt>
-    //       <dt>Tags:{story.tags}</dt>
-    //       <dt>
-    //         <Link to={"/"}>Home</Link>
-    //       </dt>
-    //     </dl>
-    //   ) : null;
-    // }
-    // return null;
   }
 }
 
