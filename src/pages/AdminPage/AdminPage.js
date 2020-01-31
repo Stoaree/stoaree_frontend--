2012599df97399eb4308febc6966dc48b9974bf2
techspeterson <tpeterson.engsci@gmail.com>
2020-01-31 14:18:31 +1100

    working on admin page

--- src/pages/AdminPage/AdminPage.js
+++ src/pages/AdminPage/AdminPage.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import axiosAPI from "../../api/stoareeAPI";
 import Question from "./Question";
-// import { connect } from "react-redux";
+import { connect } from "react-redux";
+import { setAllQuestions } from "../../redux/storyReducer";
+
+function mapStateToProps(state) {
+  return {
+    allQuestions: state.storyReducer.allQuestions
+  };
+}
+
+const mapDispatchToProps = {
+  setAllQuestions
+}
 
 class AdminPage extends React.Component {
-  state = {
-    questions: []
-  }
-
   componentDidMount() {
-    axiosAPI.get("/questions/admin/").then(response => {
-      this.setState({ questions: response.data });
-    })
+    axiosAPI.get("/questions/admin/").then(res => {
+      this.props.setAllQuestions(res.data);
+    });
   }
 
   renderQuestions = () => {
-    const { questions } = this.state;
-    if (questions) {
-      const topLevelQuestions = questions.filter(question => question.isTopLevel);
-      return topLevelQuestions.map(question => {
-        return <Question key={question._id} question={question} questions={questions} />
+    const { allQuestions } = this.props;
+    if (allQuestions) {
+      const topLevelQuestions = allQuestions.filter(question => question.isTopLevel);
+      return topLevelQuestions.map((question, index, topLevelQuestions) => {
+        const lastIndex = topLevelQuestions.length - 1
+        return <Question key={question._id} question={question} allQuestions={allQuestions} setAllQuestions={setAllQuestions} isLast={index === lastIndex} />
       });
     }
   }
@@ -31,4 +39,4 @@ class AdminPage extends React.Component {
   }
 }
 
-export default AdminPage;
+export default connect(mapStateToProps, mapDispatchToProps)(AdminPage);
--- src/pages/AdminPage/Question.js
+++ src/pages/AdminPage/Question.js
@@ -1,13 +1,69 @@
 import React from "react";
+import QuestionForm from "./QuestionForm";
+import axiosAPI from "../../api/stoareeAPI";
 
 class Question extends React.Component {
+  state = {
+    adding: false,
+    editing: false
+  }
+
   renderSubQuestions = () => {
-    const { question, questions } = this.props;
+    const { question, allQuestions } = this.props;
     if (question.subQuestions) {
-      return question.subQuestions.map(subQuestionId => {
-        const questionObj = questions.find(foundQuestion => foundQuestion._id === subQuestionId);
-        return <Question key={questionObj._id} question={questionObj} questions={questions} />
+      return question.subQuestions.map((subQuestionId, index, subQuestions) => {
+        const childQuestion = allQuestions.find(foundQuestion => foundQuestion._id === subQuestionId);
+        const lastIndex = subQuestions.length - 1;
+        return <Question key={childQuestion._id} question={childQuestion} allQuestions={allQuestions} parent={question} setAllQuestions={this.props.setAllQuestions} isLast={index === lastIndex} />
       });
+    }
+  }
+
+  onAdd = (values) => {
+    const { question, parent } = this.props;
+
+    // get the order no.
+
+    axiosAPI.post("/questions/admin", {
+      title: values.title,
+      isTopLevel: question.isTopLevel,
+      isYesOrNo: values.isYesOrNo,
+      parentQuestionId: parent ? parent._id : null
+    }).then(res => {
+      this.props.setAllQuestions(res.data);
+      this.setState({ adding: false });
+    });
+  }
+
+  cancel = () => {
+    this.setState({ adding: false, editing: false });
+  }
+
+  setAdding = () => {
+    this.setState({ adding: true });
+  }
+
+  setEditing = () => {
+    this.setState({ editing: true });
+  }
+
+  renderAddButtonOrForm = () => {
+    if (this.props.isLast) {
+      if (this.state.adding) {
+        return <QuestionForm label={"Add question"} onSubmit={this.onAdd} cancel={this.cancel} />
+      }
+      else {
+        return <button onClick={this.setAdding}>Add</button>
+      }
+    }
+  }
+
+  renderEditButtonOrForm = () => {
+    if (this.state.editing) {
+      return <QuestionForm label={"Add question"} onSubmit={this.onAdd} cancel={this.cancel} />
+    }
+    else {
+      return <button onClick={this.setEditing}>Edit</button>
     }
   }
 
@@ -16,8 +72,10 @@ class Question extends React.Component {
 
     return (
       <div style={{ marginLeft: 50 }}>
-        <h2>{title}</h2>
+        <h3>{title}</h3>
+        {this.renderEditButtonOrForm()}
         {this.renderSubQuestions()}
+        {this.renderAddButtonOrForm()}
       </div>
     )
   }
