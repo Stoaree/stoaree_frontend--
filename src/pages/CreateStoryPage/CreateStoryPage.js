8f37cf8d4b66e8b3ad6f140ebddd0a8e856c15ac
Max Kelly <max@revelthat.com>
2020-02-05 14:47:37 +1100

    small changes

--- src/pages/CreateStoryPage/CreateStoryPage.js
+++ src/pages/CreateStoryPage/CreateStoryPage.js
@@ -8,6 +8,9 @@ import { setCurrentStory } from "../../redux/storyReducer";
 import StoryForm from "../../components/StoryForm/StoryForm"
 import RecordPage from "../RecordPage/RecordPage";
 
+// CSS 
+import './CreateStoryPage.css';
+
 function mapStateToProps(state) {
   const { currentStory } = state.storyReducer;
   return {
@@ -23,10 +26,6 @@ class CreateStoryPage extends React.Component {
   state = {
     recording: false
   }
-
-  // onSubmit = (values) => {
-  //   console.log(values);
-  // }
 
   postStory = (values, url = "") => {
     axiosAPI.post("/stories", { ...values, imageURL: url }).then(response => {
@@ -83,7 +82,16 @@ class CreateStoryPage extends React.Component {
   render() {
     return (
       <div>
-        <h3>Create New Story</h3>
+        <div className="create-story-page-container">
+          <h2 className="create-story-page-text">Before we begin...</h2>
+          <p className="create-story-page-text"> Our mission is to allow you to capture the interesting stories of your loved ones, friends, local heros and more. </p>
+          <p className="create-story-page-text"> Before you being here are a few tips to help you get the most out of the interviewer and the story itself.</p>
+          <ul className="create-story-page-list-container">
+            <li className="create-story-page-text"> Ensure you're in a quiet environment where there is no background noise. </li>
+            <li className="create-story-page-text"> Encourage the interviewee to tell smaller stories along the way and dig deeper into those stories. </li>
+            <li className="create-story-page-text"> Everyone has a story lets enjoy listening to each others.  </li>
+          </ul>
+        </div>
         {this.renderFormOrQuestions()}
       </div>
     );
